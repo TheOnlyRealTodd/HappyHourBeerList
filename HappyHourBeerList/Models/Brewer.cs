1109062f3cd105c0e7e9f7e705f60c9ff152157d
Todd <toddcullum@gmail.com>
2016-08-19 23:50:02 -0700

    Added beginning of Brewer domain model, also minor Home Page tweaks

---
+++ HappyHourBeerList/Models/Brewer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HappyHourBeerList.Models
+{
+    public class Brewer
+    {
+        //Still need to add Brewer into Beer class, One-to-Many Brewer:Beers
+        public int BrewerId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string LogoUrl { get; set; }
+        public virtual Address Address { get; set; }
+        public ICollection<Beer> Beers { get; set; }
+        [Display(Name = "Last Updated")]
+        public DateTime LastUpdated { get; set; }
+
+        public DateTime? DateAdded { get; set; }
+    }
+}
